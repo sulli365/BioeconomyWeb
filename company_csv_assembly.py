7c7624de75058606dee9d299ebe314a9156baf65
Sean F. Sullivan <sfsulliv@gmail.com>
2024-07-28 18:03:08 -0400

    catching up somehow

--- company_csv_assembly.py
+++ company_csv_assembly.py
@@ -8,6 +8,9 @@ import pandas as pd
 import numpy as np
 
 
-raw_company_list_path = "C:/Users/sfsul/Coding Files/Supplementary Files/BioeconomyWebSupp/2024-07-28 raw company list.csv"
+raw_company_list_path = "C:/Users/sfsul/Coding Files/Supplementary Files/BioeconomyWebSupp/2024-07-28 raw company list utf8.csv"
 
-raw_company_list = pd.read_csv(filepath_or_buffer=raw_company_list_path, sheet_name = "Sheet1")
+raw_company_list = pd.read_csv(filepath_or_buffer=raw_company_list_path, header=0, skipinitialspace=True, on_bad_lines='warn')
+
+# Getting an error that utf8 can't decode a byte in position 396 - invalid start byte...not even sure how to fix this...
+# Moved forward by re-saving the csv as UTF8 csv in excel
